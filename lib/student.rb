5e6de4752877a47c8911888613375eafab873f1b
Andrew Williams <1williamsaj@gmail.com>
2019-10-21 23:46:03 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,8 +5,8 @@ class Student
 
   @@all = []
 
-  def initialize(student_hash) # We pass in the array as soon as we make a new student
-    @name = student_hash[:name]
+  def initialize(student_hash) # We pass in the array as soon as we make a new student.
+    @name = student_hash[:name] # We extract the value from our name key in the hash.
     @location = student_hash[:location]
 
     @@all << self
