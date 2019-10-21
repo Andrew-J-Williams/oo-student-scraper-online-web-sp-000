b115f4a0382f48d248541ffcabee4f33efe44bad
Andrew Williams <1williamsaj@gmail.com>
2019-10-21 23:46:59 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -15,7 +15,7 @@ class Student
   def self.create_from_collection(students_array)
     #binding.pry
     students_array.each do |info|
-        student = Student.new(info)
+        Student.new(info)
     end
   end
 
